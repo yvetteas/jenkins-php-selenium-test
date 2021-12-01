09581e0a0b345a7abc292e1ae067436def52f008
yvetteas <1902701@sit.singaporetech.edu.sg>
2021-12-01 15:43:52 +0800

    update add

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://localhost";
+	String url = "10.0.2.15";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
